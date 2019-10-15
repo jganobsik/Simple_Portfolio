8301b21fc748b24016b2a4fa2b15fe1f25aa37ae
Joe Ganobsik <jganobsik@gmail.com>
2019-10-15 15:54:52 -0400

    update resume, change icon to calendar

--- src/resumeData.js
+++ src/resumeData.js
@@ -1,61 +1,67 @@
 let resumeData = {
-    "imagebaseurl":"https://rbhatia46.github.io/",
-    "name": "Rahul Bhatia",
-    "role": "Frontend Developer and Data Scientist",
-    "linkedinId":"Your LinkedIn Id",
-    "skypeid": "Your skypeid",
-    "roleDescription": "I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles or simply play games in my free time.",
+    "imagebaseurl":"https://jganobsik.github.io/",
+    "name": "Joe Ganobsik",
+    "role": "Full-Stack Web Developer",
+    "linkedinId":"https://www.linkedin.com/in/jganobsik/",
+    "skypeid": "j.ganobsik",
+    "roleDescription": "I enjoy building web products using modern web frameworks, prototyping networks using blockchain technology, and building communication platforms",
     "socialLinks":[
         {
           "name":"linkedin",
-          "url":"https://www.linkedin.com/in/rahul-bhatia-67ba08121/",
+          "url":"https://www.linkedin.com/in/jganobsik/",
           "className":"fa fa-linkedin"
         },
         {
           "name":"github",
-          "url":"http://github.com/rbhatia46",
+          "url":"http://github.com/jganobsik",
           "className":"fa fa-github"
         },
         {
-          "name":"skype",
-          "url":"http://twitter.com/rbhatia46",
-          "className":"fa fa-twitter"
+          "name":"Book A Conversation",
+          "url":"http://see.myskeddy.com/jganobsik",
+          "className":"fa fa-calendar"
         }
       ],
-    "aboutme":"I am currently a pre-final year student at The LNM Institute of Information Technology and pursuing my B.Tech from here. I am a self taught Full Stack Web Developer, currently diving deeper into Machine Learning. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
-    "address":"India",
-    "website":"https://rbhatia46.github.io",
+    "aboutme":"I am a Full Stack Web Developer specializing in React, Ruby on Rails, Node and Go. I am passionate about innovation and the entrepreneurial mindset.",
+    "address":"Columbus, OH",
+    "website":"github.com/jganobsik",
     "education":[
       {
-        "UniversityName":"The LNM Insitute of Information Technology",
-        "specialization":"Some specialization",
-        "MonthOfPassing":"Aug",
-        "YearOfPassing":"2020",
-        "Achievements":"Some Achievements"
-      },
-      {
-        "UniversityName":"Some University",
-        "specialization":"Some specialization",
-        "MonthOfPassing":"Jan",
-        "YearOfPassing":"2018",
-        "Achievements":"Some Achievements"
+        "UniversityName":"Miami University",
+        "specialization":"B.S. in Business, Entrepreneurship, European Culture and Pre-Law. Completed several CS courses in Database Design, Visual Basic, SQL, and IT Infrastructure",
+        "MonthOfPassing":"Jun",
+        "YearOfPassing":"2016",
+        "Achievements":"MUDEC Distinguished Scholar, Treasurer of Delta Sigma Phi Fraternity, Farmer School of Business Crocs Shoes Case Study Competition Winner"
       }
     ],
     "work":[
       {
-        "CompanyName":"Some Company",
-        "specialization":"Some specialization",
-        "MonthOfLeaving":"Jan",
-        "YearOfLeaving":"2018",
-        "Achievements":"Some Achievements"
+        "CompanyName":"Freelance Web Developer",
+        "specialization":"Full Stack Web App Developer",
+        "MonthOfLeaving":"Current",
+        "Achievements":" Directed the development, testing and deployment of web and desktop apps from ideation to final delivery using Firebase and AWS Created pitch materials, technical documentation, and projections leading to seed round Participated in The Batchery accelerator in Berkeley, California"
       },
       {
-        "CompanyName":"Some Company",
-        "specialization":"Some specialization",
-        "MonthOfLeaving":"Jan",
+        "CompanyName":"Tecille Technology Prvt. Ltd.",
+        "specialization":"Head of Product",
+        "MonthOfLeaving":"May",
         "YearOfLeaving":"2018",
-        "Achievements":"Some Achievements"
-      }
+        "Achievements":" Directed the development, testing and deployment of web and desktop apps from ideation to final delivery using Firebase and AWS Created pitch materials, technical documentation, and projections leading to seed round Participated in The Batchery accelerator in Berkeley, California"
+      },
+      {
+        "CompanyName":"Equitas Health",
+        "specialization":"DevOps Contractor",
+        "MonthOfLeaving":"November",
+        "YearOfLeaving":"2016",
+        "Achievements":"Formulated and implemented inventory tracking system to tie over $1m in assets to grants and enhance resource visibility"
+      },
+      {
+        "CompanyName":"T. Marzetti Co",
+        "specialization":"DevOps Intern",
+        "MonthOfLeaving":"August",
+        "YearOfLeaving":"2015",
+        "Achievements":"Facilitated the migration of over 500 PCs from Windows XP to Windows 7 via SCCM, per- forming manual installation when necessary, and coordinated a nationwide rollout of VoIP software across 15+ plants and locations"
+      },
     ],
     "skillsDescription":"Your skills here",
     "skills":[
